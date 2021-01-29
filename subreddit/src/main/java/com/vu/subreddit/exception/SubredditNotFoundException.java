3f0588dbc0f5ead0a376646bacd1089f8cd019a3
Anh Vu <vuha.bhsoft@gmail.com>
2021-01-29 14:47:22 +0700

    nothing

--- subreddit/src/main/java/com/vu/subreddit/exception/SubredditNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.vu.subreddit.exception;
-
-public class SubredditNotFoundException extends RuntimeException {
-    public SubredditNotFoundException(String message) {
-        super(message);
-    }
-}
